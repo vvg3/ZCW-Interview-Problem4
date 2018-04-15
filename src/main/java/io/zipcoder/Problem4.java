8bcd28fbfa9fb5d8d75386351d9b8399564966b1
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-15 11:38:21 -0400

    4 done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,51 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    private Map<Character, Integer> characterMap;
+
+    public Problem4() {
+        characterMap = new HashMap<Character, Integer>();
+    }
+
+
+    public String checkForPalindrome(String input) {
+        String answer = null;
+        buildMap(input);
+
+        if (countOddOccurringCharacters(characterMap) > 1) {
+            answer = "NO";
+        } else {
+            answer = "YES";
+        }
+
+        return answer;
+    }
+
+
+    private void buildMap(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (characterMap.containsKey(input.charAt(i))) {
+                characterMap.put(input.charAt(i), characterMap.get(input.charAt(i)) + 1);
+            } else {
+                characterMap.put(input.charAt(i), 1);
+            }
+        }
+    }
+
+    private int countOddOccurringCharacters(Map<Character, Integer> aMap) {
+        int countOfOdds = 0;
+
+        for (Map.Entry<Character, Integer> entry : aMap.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                countOfOdds++;
+            }
+        }
+        return countOfOdds;
+    }
+
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,41 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    private Problem4 tester;
+
+
+    @Before
+    public void setup() {
+        tester = new Problem4();
+    }
+
+    @Test
+    public void canBePalindromeTest1() {
+        String input = "aaabbbb";
+        String expected = "YES";
+        String actual = tester.checkForPalindrome(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canBePalindromeTest2() {
+        String input = "cdefghmnopqrstuvw";
+        String expected = "NO";
+        String actual = tester.checkForPalindrome(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canBePalindromeTest3() {
+        String input = "cdcdcdcdeeeef";
+        String expected = "YES";
+        String actual = tester.checkForPalindrome(input);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
